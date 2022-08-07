547e4e3108569b5dae573e0bc78083fc68562080
Darryl <darrylpeterson@Darryls-MacBook-Pro.local>
2022-08-06 20:12:03 -0400

    commit

---
+++ 2script.js
@@ -0,0 +1,59 @@
+// questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Tool Point",
+      "Hyper Text Markup Language",
+      "Hyper Text Macro Language",
+      "Hyper Tool Major Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Counting Style Sheet",
+      "Comment Style Sheet",
+      "Cactus Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does API stand for?",
+    answer: "Application programming interface",
+    options: [
+      "Actual Preprocessor interface",
+      "Active programming interface",
+      "Application programming interface",
+      "Ant Preprocessor interface"
+    ]
+  },
+    {
+    numb: 4,
+    question: "APIs allow __",
+    answer: "two applications to talk to each other",
+    options: [
+      "two applications to talk to each other",
+      "Java to java in the java of java",
+      "applications to ignore each other",
+      "applications to deactivate"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does JSON stand for?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "JavaScript Open Notation",
+      "JavaScript Object Network",
+      "JavaScript Object Notation",
+      "JavaScript Outer Notation"
+    ]
+  },
+
+];
